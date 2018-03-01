21392ca552ce6609f67156fa32b27903ee3de229
yasuhiro-take <take.yasuhiro.120@gmail.com>
2018-03-01 15:22:51 +0900

    handle pixelized values and (X,Y,Z) headings by default.

--- src/imagejplugin/kneectanalyzer/RTBoundary.java
+++ src/imagejplugin/kneectanalyzer/RTBoundary.java
@@ -2,6 +2,7 @@ package imagejplugin.kneectanalyzer;
 
 import java.util.ArrayList;
 
+import ij.measure.Calibration;
 import ij.measure.ResultsTable;
 
 public class RTBoundary {
@@ -14,12 +15,19 @@ public class RTBoundary {
 	public int getSplitX() {
 		if (rt == null) return -1;
 		
+		int nrx = 0, cnt = 0;
 		for (int i = 0; i < rt.size(); i++) {
-			if (rt.getLabel(i).equals("FemSplitX")) {
-				return (int)rt.getValue("PixelX", i);
+			String label = rt.getLabel(i);
+			if (label.equals("FemSplitX")) {
+				return (int)rt.getValue("X", i);
+			} else if (label.equals("notchroof")) {
+				nrx += (int)rt.getValue("PixelX", i);
+				cnt++;
 			}
 		}
 		
+		if (cnt > 0)
+			return nrx / cnt;
 		return -1;
 	}
 	
@@ -31,7 +39,7 @@ public class RTBoundary {
 		
 		for (int i = 0; i < rt.size(); i++) {
 			if (rt.getLabel(i).equals(type)) {
-				int v = (int)rt.getValue("Slice", i);
+				int v = (int)rt.getValue("Z", i);
 				if (v < z) {
 					imin = i;
 					z = v;
@@ -50,7 +58,7 @@ public class RTBoundary {
 		
 		for (int i = 0; i < rt.size(); i++) {
 			if (rt.getLabel(i).equals(type)) {
-				int v = (int)rt.getValue("Slice", i);
+				int v = (int)rt.getValue("Z", i);
 				if (v > z) {
 					imax = i;
 					z = v;
@@ -63,10 +71,11 @@ public class RTBoundary {
 	
 	public int find(int type, int z) {
 		if (rt == null) return -1;
+		String label = BoundaryData.TYPESTRING[type];
 		
 		for (int i = 0; i < rt.size(); i++) {
-			if (rt.getLabel(i).equals(type)) {
-				int v = (int)rt.getValue("Slice", i);
+			if (rt.getLabel(i).equals(label)) {
+				int v = (int)rt.getValue("Z", i);
 				if (z == v)
 					return i;
 			}
@@ -79,7 +88,7 @@ public class RTBoundary {
 		int imin = findProximal(type);
 		
 		if (imin != -1) {
-			int z = (int)rt.getValue("Slice", imin) - 1;
+			int z = (int)rt.getValue("Z", imin);
 			int typevalue = BoundaryData.getType(type);
 			
 			return new BoundaryData(typevalue, z, getRect(imin));
@@ -91,7 +100,7 @@ public class RTBoundary {
 		int imax = findDistal(type);
 		
 		if (imax != -1) {
-			int z = (int)rt.getValue("Slice", imax) - 1;
+			int z = (int)rt.getValue("Z", imax);
 			int typevalue = BoundaryData.getType(type);
 			
 			return new BoundaryData(typevalue, z, getRect(imax));
@@ -119,7 +128,7 @@ public class RTBoundary {
 		
 		BoundaryList bl = new BoundaryList();
 		for (int i = 0; i < rt.size(); i++) {
-			int s = (int)rt.getValue("Slice", i);
+			int s = (int)rt.getValue("Z", i);
 			if (s == z) 
 				bl.bdlist.add(getOne(i));
 				
@@ -128,18 +137,18 @@ public class RTBoundary {
 		return bl;
 	}
 	
-	public int getProximalSlice(String type) {
+	public int getProximalZ(String type) {
 		int row = findProximal(type);
 		if (row != -1)
-			return (int)rt.getValue("Slice", row);
+			return (int)rt.getValue("Z", row);
 		return -1;
 		
 	}
 	
-	public int getDistalSlice(String type) {
+	public int getDistalZ(String type) {
 		int row = findDistal(type);
 		if (row != -1)
-			return (int)rt.getValue("Slice", row);
+			return (int)rt.getValue("Z", row);
 		return -1;
 		
 	}
@@ -154,7 +163,7 @@ public class RTBoundary {
 	}
 	
 	public BoundaryData getOne(int row) {
-		int z = (int)rt.getValue("Slice", row) - 1;
+		int z = (int)rt.getValue("Z", row);
 		String type = rt.getLabel(row);
 		int typevalue = BoundaryData.getType(type);
 		
@@ -166,11 +175,42 @@ public class RTBoundary {
 		if (rt == null) return ;
 		
 		for (int i = 0; i < rt.size(); i++) {
-			if (rt.getLabel(i).equals(BoundaryData.TYPESTRING[BoundaryData.NOTCHROOF])) {
-				double y = rt.getValue("PixelY", i);
-				double z = rt.getValue("Slice", i) - 1;
+			if (rt.getLabel(i).equals(BoundaryData.TYPESTRING[BoundaryData.NOTCH])) {
+				double y = rt.getValue("Y", i);
+				double z = rt.getValue("Z", i);
 				yl.add(y); zl.add(z);
 			}
+		}
+	}
+	
+	public boolean isReal() {
+		if (rt == null) return false;
+		
+		String[] heads = rt.getHeadings();
+		if (heads == null) return false;
+		
+		for (int i = 0; i < heads.length; i++)
+			if (heads[i].startsWith("Pixel"))
+				return true;
+		return false;
+	}
+	
+	public void real2px(Calibration cal) {
+		if (rt == null) return;
+		
+		for (int i = rt.size() - 1; i >= 0; i--) {
+			double x = rt.getValue("X", i);
+			double y = rt.getValue("Y", i);
+			double w = rt.getValue("Width", i);
+			double h = rt.getValue("Height", i);
+			
+			Rect r = new Rect(x, y, w, h);
+			r.real2px(cal);
+			
+			rt.setValue("X", i, r.x);
+			rt.setValue("Y", i, r.y);
+			rt.setValue("Width", i, r.w);
+			rt.setValue("Height", i, r.h);
 		}
 	}
 }
