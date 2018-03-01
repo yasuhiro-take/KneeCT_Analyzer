f6ed3960b3afae2e8ce207a724b60ff99ff0fadd
yasuhiro-take <take.yasuhiro.120@gmail.com>
2018-03-01 15:21:05 +0900

    Deprecate pointlist transformation. Add WW/WL modifying method by mouse dragging.

--- src/imagejplugin/kneectanalyzer/OrthogonalTransformer.java
+++ src/imagejplugin/kneectanalyzer/OrthogonalTransformer.java
@@ -1,27 +1,21 @@
 package imagejplugin.kneectanalyzer;
 
 import ij.*;
-
 import ij.process.*;
 import ij.measure.*;
 import ij.gui.*;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.Vector;
 
+import ij.plugin.PlugIn;
 import ij.plugin.filter.ExtendedPlugInFilter;
 import ij.plugin.filter.PlugInFilterRunner;
 
-public class OrthogonalTransformer /* implements PlugIn */ {
-	/*
-	@Override
-	static public void run(String arg) { // plug-in run
-		ImagePlus imp = WindowManager.getCurrentImage();   
-		
-		runMain(imp, arg, null, null);	
-	}
-	*/
-	
-	private mPointList plConv = null;
+public class OrthogonalTransformer implements PlugIn {
 	private ImagePlus lastImpAx;
 	private ImagePlus impZ[] = new ImagePlus[3];
 	
@@ -29,36 +23,24 @@ public class OrthogonalTransformer /* implements PlugIn */ {
 	private static final int I_CORONAL = 1;
 	private static final int I_SAGITTAL = 2;
 	
-	public int run(String wintitle, mPointList pl) {
-		ImagePlus imp = WindowManager.getImage(wintitle);
+	@Override public void run(String arg) { 
+		ImagePlus imp = WindowManager.getCurrentImage();   
+		if (imp == null) {
+			IJ.noImage();
+			return;
+		}
 		
-		if (imp == null)  // when wintitle == null || wintitle is closed.
-			if ((imp = WindowManager.getCurrentImage()) == null)
-				return -1;
-		
-		imp.killRoi();
-		
-		if (pl != null)
-			plConv = pl.duplicate();
-		
-		lastImpAx = imp;
-		
-		return runMain(imp, null); // Cancel: -1, Finish: 0
+		directRun(imp, arg);
 	}
 	
 	public ImagePlus getTransformedImage() {
 		return lastImpAx;
 	}
-	public mPointList getTransformedPointList() {
-		return plConv;
-	}
-		
-	private int runMain(ImagePlus impOriginal, String arg) {
+	
+	public int directRun(ImagePlus impOriginal, String arg) {
 		ImagePlus impstack[] = new ImagePlus[3];
 		
-		double angle[] = null;
-		if (plConv != null && plConv.size() >= 2)
-			angle = getAnglesBy2P(plConv);
+		lastImpAx = impOriginal;
 		
 		notice("Creating orthogonal projections... \nThis takes some moments.");
 		
@@ -70,7 +52,7 @@ public class OrthogonalTransformer /* implements PlugIn */ {
 		
 		this.orthoStacksToZProjects(impstack); // this.impZ updated, impstack[1,2] closed.
 		
-		OrthogonalTransformerFilter mf = new OrthogonalTransformerFilter(this.impZ, angle);
+		OrthogonalTransformerFilter mf = new OrthogonalTransformerFilter(this.impZ);
 		mf.impOrthoView.show();
 		mf.impOrthoView.getWindow().toFront();
 		
@@ -83,6 +65,7 @@ public class OrthogonalTransformer /* implements PlugIn */ {
 			lastImpAx = null;
 		}
 		IJX.forceClose(mf.impOrthoView);
+		IJX.rename(lastImpAx, "Aligned " + impOriginal.getTitle());
 		
 		return r;
 	}
@@ -91,7 +74,7 @@ public class OrthogonalTransformer /* implements PlugIn */ {
 		for (;;) {
 			WindowManager.setCurrentWindow(mf.impOrthoView.getWindow());
 			
-			new PlugInFilterRunner(mf, "commandname", arg);
+			new PlugInFilterRunner(mf, null, arg);
 			
 			if (mf.endStatus == mf.END_CANCEL)
 				return -1;
@@ -104,7 +87,7 @@ public class OrthogonalTransformer /* implements PlugIn */ {
 					
 					ImagePlus impstack[] = createTransformedStacks(impAx, a, f); 
 					
-					this.transformPoints(a, f, impstack); // this.plConv is transformed
+					//this.transformPoints(a, f, impstack); // this.plConv is transformed
 					this.orthoStacksToZProjects(impstack); // this.impZ updated, impstack[1,2] closed.
 					
 					impAx = impstack[I_AXIAL];
@@ -118,8 +101,7 @@ public class OrthogonalTransformer /* implements PlugIn */ {
 				if (mf.endStatus == mf.END_OK)
 					return 0;
 				else
-					arg = null;  // set angle[3] = { 0, 0, 0 } in mf;
-				
+					arg = null;
 			}
 		}
 	}
@@ -154,7 +136,318 @@ public class OrthogonalTransformer /* implements PlugIn */ {
 		return impstack;
 	}
 	
-	private void transformPoints(final double angle[], final boolean flip[], final ImagePlus impstack[]) {
+	private void orthoStacksToZProjects(ImagePlus impstack[]) {
+		for (int i = 0; i < 3; i++) {
+			IJX.forceClose(this.impZ[i]);
+			
+			this.impZ[i] = IJX.zproject(impstack[i]); // TODO: start and end slice
+			
+			if (i > 0) {
+				IJX.forceClose(impstack[i]);
+				impstack[i] = null;
+			}
+		}
+	}
+	
+	static private void notice(String message) {
+		//IJ.log(message);
+		IJIF.notice(message);
+	}
+	
+	static private double[] getAnglesBy2P(mPointList pl) {
+		double ret[] = new double[3];
+		
+		Line l1 = new Line(pl.get(0).x, pl.get(0).y, pl.get(1).x, pl.get(1).y);
+		ret[0] = l1.getAngle(); // Axial angle
+		
+		Line l2 = new Line(pl.get(0).x, pl.get(0).z, pl.get(1).x, pl.get(1).z);
+		ret[1] = l2.getAngle(); // Coronal angle
+		
+		ret[2] = 0;
+		
+		return ret;
+	}
+}
+
+/**
+* class OrthogonalTransformerFilter - impl. ExtendedPluginFilter, DialogListener 
+* 
+* This class visualizes projections in three orthogonal plane, i.e., axial, coronal      
+* and sagittal planes, and provide an interactive interface to rotate and flip them
+* with preview.
+* 
+* Input (at constructor)
+*   impZ[]: three non-stack images as ImagePlus (projections of orthogonal stacks)
+*   angles[]: three initial angles for rotation
+* Output: (partly as class member variables).
+*   impOrthoView - ImagePlus; an image showing three impZ[] in one, which may be 
+*     transformed according to user-inputs through the dialog (when preview is ON).   
+*   angle[] - double; user-specified values through the dialog
+*   flip[] - boolean; user-specified values through the dialog
+* 
+* Overall, the original stack of axial slices is supposed to be three-dimensionally
+* transformed according to the user-inputs, while this plugin-filter only provides     
+* UI to show projections of orthogonal stacks with a temporal transformation.
+* The plugin-filter invoker should transform the stacks according to the user-inputs.
+* When this plugin-filter ends with END_APPLY (when APPLY button is clicked),  
+* the invoker should reconstruct projections of transformed orthogonal stacks, and
+* re-run this plugin-filter.  
+* 
+* @author Yasuhiro Take, M.D., Ph.D. - Osaka Univ. Graduate School of Medicine
+* 
+* License: GPL v3. ABSOLUTELY NO WARRANTY.
+*
+*/
+
+class OrthogonalTransformerFilter implements ExtendedPlugInFilter, DialogListener {
+	
+	private double angle[] = new double[4];
+	private boolean flip[] = new boolean[3];
+	
+	private ImagePlus impZ[];
+	private int px[] = new int[4], width[] = new int[4], height[] = new int[4];
+
+	public final int END_OK = 1, END_APPLY=2, END_CANCEL=3;
+	public int endStatus;
+	public ImagePlus impOrthoView;
+	public double minPx, maxPx;
+	public double yscale;
+	
+	private void init(ImagePlus impZ[]) {
+		this.impZ = impZ;
+		
+		this.impOrthoView = createOrthoView(impZ);
+		this.minPx = impOrthoView.getProcessor().getMin();
+		this.maxPx = impOrthoView.getProcessor().getMax();
+		this.yscale = impOrthoView.getWidth() / impOrthoView.getHeight();
+	}
+	
+	public OrthogonalTransformerFilter(ImagePlus impZ[], final double angle[], final boolean flip[]) {
+		init(impZ);
+		
+		for (int i = 0; i < 3; i++) {
+			this.angle[i] = (angle != null) ? angle[i] : 0;
+			this.flip[i] = (flip != null) ? flip[i] : false;
+		}
+	}
+	
+	public OrthogonalTransformerFilter(ImagePlus impZ[]) {
+		init(impZ);
+		
+		for (int i = 0; i < 3; i++) {
+			this.angle[i] = 0;
+			this.flip[i] = false;
+		}
+	}
+	
+	@Override public int setup(String arg, ImagePlus imp) {
+		return DOES_ALL;
+	}
+
+	@Override public void run(ImageProcessor ip) {
+		refreshOrthoView();	
+	}
+	
+	@Override public int showDialog(ImagePlus imp, String command, PlugInFilterRunner pfr) {
+		this.endStatus = 0;
+		
+		OTMouseListener otml = new OTMouseListener(this);
+		ImageCanvas ic = imp.getCanvas();
+		ic.addMouseMotionListener(otml);
+		ic.addMouseListener(otml);
+		
+		IJ.setTool(Toolbar.HAND);
+		
+		String items[] = { "Axial", "Coronal", "Sagittal" };
+				
+		NonBlockingGenericDialog gd = new NonBlockingGenericDialog("Orthogonal Transformation Dialog");
+		
+		gd.addMessage("Correct alignment by rotation and flipping in orhtogonal planes.\n" +
+				"You can repeat correction by 'Apply' button, until pressing 'Finish' button.\n"+
+				"Preview shows a temporally rotated/flipped images in 3-plane projections.");
+		
+		for (int i = 0; i < 3; i++)
+			gd.addNumericField(items[i] + " angle:", angle[i], 3);
+		
+		gd.setInsets(10, 20, 0);
+
+		gd.addCheckbox("Flip Medial-Lateral", false);
+		gd.addCheckbox("Flip Proximal-Distal", false);
+		gd.addCheckbox("Flip Anterior-Posterior", false);
+		
+		gd.setInsets(10, 40, 0);
+		
+		gd.addPreviewCheckbox(pfr);
+		// pfr.setDialog(gd); --- called at gd.showDialog() 
+		// at pfr.setDIalog(), gd.AddDialogListener(this) is called.
+		gd.addDialogListener(this);
+		gd.enableYesNoCancel("Finish", "Apply");
+		gd.getPreviewCheckbox().setState(true);
+		gd.showDialog();
+		
+		ic.removeMouseMotionListener(otml);
+		ic.removeMouseListener(otml);
+		
+		if (gd.wasOKed())
+			this.endStatus = END_OK;		
+		 else if (gd.wasCanceled())
+			 this.endStatus = END_CANCEL;
+		 else
+			 this.endStatus = END_APPLY;
+		
+		return 0;
+	}
+
+	@Override public void setNPasses(int nPasses) {}
+	
+	public boolean dialogItemChanged(GenericDialog gd, AWTEvent e) {
+		
+		for (int i = 0; i < 3; i++) {
+			double v = gd.getNextNumber();
+			this.angle[i] = Double.isNaN(v) ? 0 : v;
+		}
+
+		for (int i = 0; i < 3; i++)
+			this.flip[i] = gd.getNextBoolean();
+		
+		return true;
+	}
+	
+	public double[] getAngles() {
+		return angle;
+	}
+	
+	public boolean[] getFlipflags() {
+		return flip;
+	}
+
+
+	public boolean isChanged() {
+		if (angle[0] != 0 || angle[1] != 0 || angle[2] != 0 ||
+			flip[0] || flip[1] || flip[2])
+			return true;
+		else
+			return false;
+	}		
+	
+	public void clearInput() {
+		for (int i = 0;i < 3; i++) {
+			angle[i] = 0.0;
+			flip[i] = false;
+		}
+	}
+	
+	private ImagePlus createOrthoView(ImagePlus impZ[]) {
+		int px0 = 0;
+			
+		for (int i = 0; i < 3; i++) {
+			int w = impZ[i].getWidth();
+			int h = impZ[i].getHeight();
+			
+			this.px[i] = px0;
+			this.width[i] = w;
+			this.height[i] = h;
+			
+			px0 += w; 			
+		}
+		
+		return IJX.createCombinedImage("Orthogonal Projection", impZ);
+	}
+	
+	private void refreshOrthoView() {
+		for (int i = 0; i < 3; i++) {
+			ImageProcessor ipZ = impZ[i].getProcessor();
+			ipZ.reset();
+			
+			switch(i) {
+				case 0:
+					if (flip[0]) ipZ.flipHorizontal();
+					if (flip[2]) ipZ.flipVertical();
+					break;
+				case 1:
+					if (flip[0]) ipZ.flipHorizontal();
+					if (flip[1]) ipZ.flipVertical();
+					break;
+				case 2:
+					if (flip[1]) ipZ.flipVertical();
+					if (flip[2]) ipZ.flipHorizontal(); 
+					break;
+			}
+			
+			ipZ.rotate(angle[i]);
+			
+			impZ[i].killRoi();
+			impZ[i].copy();
+			IJ.wait(50);
+			
+			impOrthoView.setRoi(this.px[i], 0, this.width[i], this.height[i]);
+			impOrthoView.paste();
+			impOrthoView.killRoi();
+		}
+		
+		//impOrthoView.getProcessor().setMinAndMax(minPx, maxPx);
+		
+		impOrthoView.updateAndDraw();
+	}
+	
+	public void updateOrthoView(ImagePlus impZ[]) {
+		this.impZ = impZ;
+		for (int i = 0; i < 3; i++)
+			impZ[i].getProcessor().snapshot();
+		IJ.wait(50);
+		
+		refreshOrthoView();
+	
+	}
+}
+
+class OTMouseListener implements MouseMotionListener, MouseListener {
+	private int startX, startY;
+	private OrthogonalTransformerFilter otf;
+	
+	OTMouseListener(OrthogonalTransformerFilter otf) {
+		this.otf = otf;
+	}
+	
+	@Override public void mouseDragged(MouseEvent e) {
+		double wl = (otf.maxPx + otf.minPx) / 2;
+		double ww = (otf.maxPx - otf.minPx);
+		
+		int x = e.getX();
+		int y = e.getY();
+		int dx = x - startX; int dy = y - startY;  
+		wl += dx;
+		ww += dy * otf.yscale;
+		
+		otf.maxPx = wl + ww / 2;
+		otf.minPx = wl - ww / 2;
+		
+		startX = x;
+		startY = y;
+		
+		otf.impOrthoView.getProcessor().setMinAndMax(otf.minPx, otf.maxPx);
+		otf.impOrthoView.updateAndDraw();
+		
+		e.consume();
+	}
+
+	@Override public void mouseMoved(MouseEvent e) {}
+
+	@Override public void mouseClicked(MouseEvent e) {}
+
+	@Override public void mousePressed(MouseEvent e) {
+		startX = e.getX();
+		startY = e.getY();
+		e.consume();
+	}
+
+	@Override public void mouseReleased(MouseEvent e) {}
+	@Override public void mouseEntered(MouseEvent e) {}
+	@Override public void mouseExited(MouseEvent e) {}
+}
+
+/*
+  private void transformPoints(final double angle[], final boolean flip[], final ImagePlus impstack[]) {
 		if (plConv == null) 
 			return;
 		
@@ -242,262 +535,6 @@ public class OrthogonalTransformer /* implements PlugIn */ {
 			plConv.add(name[i], x[i], y[i], z[i]);
 	}
 	
-	
-	
-	private void orthoStacksToZProjects(ImagePlus impstack[]) {
-		for (int i = 0; i < 3; i++) {
-			IJX.forceClose(this.impZ[i]);
-			
-			this.impZ[i] = IJX.zproject(impstack[i]); // TODO: start and end slice
-			
-			if (i > 0) {
-				IJX.forceClose(impstack[i]);
-				impstack[i] = null;
-			}
-		}
-	}
-	
-	
-	static private void notice(String message) {
-		//IJ.log(message);
-		IJIF.notice(message);
-	}
-	
-	static private double[] getAnglesBy2P(mPointList pl) {
-		double ret[] = new double[3];
-		
-		Line l1 = new Line(pl.get(0).x, pl.get(0).y, pl.get(1).x, pl.get(1).y);
-		ret[0] = l1.getAngle(); // Axial angle
-		
-		Line l2 = new Line(pl.get(0).x, pl.get(0).z, pl.get(1).x, pl.get(1).z);
-		ret[1] = l2.getAngle(); // Coronal angle
-		
-		ret[2] = 0;
-		
-		return ret;
-	}
-}
-
-/**
-* class OrthogonalTransformerFilter - impl. ExtendedPluginFilter, DialogListener 
-* 
-* This class visualizes projections in three orthogonal plane, i.e., axial, coronal      
-* and sagittal planes, and provide an interactive interface to rotate and flip them
-* with preview.
-* 
-* Input (at constructor)
-*   impZ[]: three non-stack images as ImagePlus (projections of orthogonal stacks)
-*   angles[]: three initial angles for rotation
-* Output: (partly as class member variables).
-*   impOrthoView - ImagePlus; an image showing three impZ[] in one, which may be 
-*     transformed according to user-inputs through the dialog (when preview is ON).   
-*   angle[] - double; user-specified values through the dialog
-*   flip[] - boolean; user-specified values through the dialog
-* 
-* Overall, the original stack of axial slices is supposed to be three-dimensionally
-* transformed according to the user-inputs, while this plugin-filter only provides     
-* UI to show projections of orthogonal stacks with a temporal transformation.
-* The plugin-filter invoker should transform the stacks according to the user-inputs.
-* When this plugin-filter ends with END_APPLY (when APPLY button is clicked),  
-* the invoker should reconstruct projections of transformed orthogonal stacks, and
-* re-run this plugin-filter.  
-* 
-* @author Yasuhiro Take, M.D., Ph.D. - Osaka Univ. Graduate School of Medicine
-* 
-* License: GPL v3. ABSOLUTELY NO WARRANTY.
-*
-*/
-
-class OrthogonalTransformerFilter implements ExtendedPlugInFilter, DialogListener {
-	
-	private double angle[] = new double[4];
-	private boolean flip[] = new boolean[3];
-	
-	private ImagePlus impZ[];
-	private int px[] = new int[4], width[] = new int[4], height[] = new int[4];
-	
-	private double initialAngle[];
-	
-	public final int END_OK = 1, END_APPLY=2, END_CANCEL=3;
-	public int endStatus;
-	public ImagePlus impOrthoView;
-	
-	public OrthogonalTransformerFilter(ImagePlus impZ[], final double angle[]) {
-		this.impZ = impZ;
-		
-		this.impOrthoView = createOrthoView(impZ);
-		
-		if (angle != null && angle.length >= 3)
-			this.initialAngle = angle.clone();
-		else
-			this.initialAngle = null;
-	}
-	
-	@Override
-	public int setup(String arg, ImagePlus imp) {
-		if (arg != null) { 
-			String l[] = arg.split(" ");
-			for (int i = 0; i < l.length; i++)
-				this.angle[i] = Double.parseDouble(l[i]);
-			for (int i = l.length; i < 4; i++)
-				this.angle[i] = 0;
-		} else {
-			if (this.initialAngle != null) {
-				for (int i = 0; i < this.initialAngle.length; i++)
-					this.angle[i] = this.initialAngle[i];
-				for (int i = this.initialAngle.length; i < 4; i++)
-					this.angle[i] = 0;
-				
-				this.initialAngle = null;
-			} else {
-				for (int i = 0; i < 4; i++)
-					this.angle[i] = 0.0;
-			}
-		}
-
-		for (int i = 0; i < 3; i++)
-			this.flip[i] = false;
-		
-		return DOES_ALL;
-	}
-
-	@Override
-	public void run(ImageProcessor ip) {
-		refreshOrthoView();	
-	}
-
-	@Override
-	public int showDialog(ImagePlus imp, String command, PlugInFilterRunner pfr) {
-		this.endStatus = 0;
-		
-		String items[] = { "Axial", "Coronal", "Sagittal" };
-				
-		GenericDialog gd = new GenericDialog("Orthogonal Transformation Dialog");
-		
-		gd.addMessage("Correct alignment by rotation and flipping in orhtogonal planes.\n" +
-				"You can repeat correction by 'Apply' button, until pressing 'Finish' button.\n"+
-				"Preview shows a temporally rotated/flipped images in 3-plane projections.");
-		
-		for (int i = 0; i < 3; i++)
-			gd.addNumericField(items[i] + " angle:", angle[i], 3);
-		
-		gd.setInsets(10, 20, 0);
-
-		gd.addCheckbox("Flip Medial-Lateral", false);
-		gd.addCheckbox("Flip Proximal-Distal", false);
-		gd.addCheckbox("Flip Anterior-Posterior", false);
-		
-		gd.setInsets(10, 40, 0);
-		
-		gd.addPreviewCheckbox(pfr);
-		// pfr.setDialog(gd); --- called at gd.showDialog() 
-		// at pfr.setDIalog(), gd.AddDialogListener(this) is called.
-		gd.addDialogListener(this);
-		gd.enableYesNoCancel("Finish", "Apply");
-		gd.getPreviewCheckbox().setState(true);
-		gd.showDialog();
-		
-		if (gd.wasOKed())
-			this.endStatus = END_OK;		
-		 else if (gd.wasCanceled())
-			 this.endStatus = END_CANCEL;
-		 else
-			 this.endStatus = END_APPLY;
-		
-		return 0;
-	}
-
-	@Override public void setNPasses(int nPasses) {}
-	
-	public boolean dialogItemChanged(GenericDialog gd, AWTEvent e) {
-		for (int i = 0; i < 3; i++)
-			this.angle[i] = gd.getNextNumber();
-
-		for (int i = 0; i < 3; i++)
-			this.flip[i] = gd.getNextBoolean();
-		
-		return true;
-	}
-	
-	public double[] getAngles() {
-		return angle;
-	}
-	
-	public boolean[] getFlipflags() {
-		return flip;
-	}
-
-
-	public boolean isChanged() {
-		if (angle[0] != 0 || angle[1] != 0 || angle[2] != 0 ||
-			flip[0] || flip[1] || flip[2])
-			return true;
-		else
-			return false;
-	}		
-	
-	public void clearInput() {
-		for (int i = 0;i < 3; i++) {
-			angle[i] = 0.0;
-			flip[i] = false;
-		}
-	}
-	
-	private ImagePlus createOrthoView(ImagePlus impZ[]) {
-		int px0 = 0;
-			
-		for (int i = 0; i < 3; i++) {
-			int w = impZ[i].getWidth();
-			int h = impZ[i].getHeight();
-			
-			this.px[i] = px0;
-			this.width[i] = w;
-			this.height[i] = h;
-			
-			px0 += w; 			
-		}
-		
-		return IJX.createCombinedImage("Orthogonal Projection", impZ);
-	}
-	
-	private void refreshOrthoView() {
-		for (int i = 0; i < 3; i++) {
-			ImageProcessor ipZ = impZ[i].getProcessor();
-			ipZ.reset();
-			ipZ.rotate(angle[i]);
-			
-			switch(i) {
-				case 0:
-					if (flip[0]) ipZ.flipHorizontal();
-					if (flip[2]) ipZ.flipVertical();
-					break;
-				case 1:
-					if (flip[0]) ipZ.flipHorizontal();
-					if (flip[1]) ipZ.flipVertical();
-					break;
-				case 2:
-					if (flip[1]) ipZ.flipVertical();
-					if (flip[2]) ipZ.flipHorizontal();
-					break;
-			}
-			
-			impZ[i].killRoi();
-			impZ[i].copy();
-			impOrthoView.setRoi(this.px[i], 0, this.width[i], this.height[i]);
-			impOrthoView.paste();
-			impOrthoView.killRoi();
-		}
-		
-		impOrthoView.updateAndDraw();
-	}
-	
-	public void updateOrthoView(ImagePlus impZ[]) {
-		this.impZ = impZ;
-		for (int i = 0; i < 3; i++)
-			impZ[i].getProcessor().snapshot();
-		
-		refreshOrthoView();
-	}
-}
+	*/
 
 
